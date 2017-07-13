cea2ccc4dfc5054251c425af641aaa5a9c614536
Nicolas DELEPINE <Nd.delepine@gmail.com>
2017-07-13 17:29:06 +0200

    Markdown and paragraph titles as Python comments

--- zeppelinJsonToPy.py
+++ zeppelinJsonToPy.py
@@ -21,30 +21,39 @@ def zeppelinJsonToPy(fileInput, fileOutput):
     for paragraph in data["paragraphs"]:
         paragraphText = paragraph["text"]
 
-        #If it's a PySpark code, we append it to the output
+        try:
+            # We write the paragraphe title
+            paragraphTitle = paragraph["title"]
+            out += "#" + "-" * (len(paragraphTitle) - 1) + "\n"
+            out += "#" + paragraphTitle + "\n"
+            out += "#" + "-" * (len(paragraphTitle) - 1) + "\n" * 2
+        except:
+            print("")
+
+        # If it's a PySpark code, we append it to the output
         if paragraphText[0:len("%pyspark")] == "%pyspark":
             out += paragraphText[len("%pyspark"):]
             out += "\n"
             out += "\n" * 2
-        #If it's a markdown file, we comment it and append it to the output
+        # If it's a markdown file, we comment it and append it to the output
         if paragraphText[0:len("%md")] == "%md":
             lines = paragraphText.splitlines(True)
             for line in lines[1:]:
-                #If its a main title that starts with '#', we surround it by '#'
-                if ((line[0] == "#") and (line[1] != "#")) :
+                # If its a main title that starts with '#', we surround it by '#'
+                if line.startswith("#"):
                     out += "#" * len(line) + "\n"
-                    out +=  line.replace("\n","#") + "\n"
+                    out += line.replace("\n", "#") + "\n"
                     out += "#" * len(line) + "\n"
-                #Else if it's a amin title that starts with <h1>, we replace the balises by '#' and surround it with '#'
-                elif line.startswith("<h1>") :
+                # Else if it's a amin title that starts with <h1>, we replace the balises by '#' and surround it with '#'
+                elif line.startswith("<h1>"):
                     out += "#" * (len(line) - 9) + "\n"
-                    out += line.replace("<h1>","#").replace("</h1>", "#")
+                    out += line.replace("<h1>", "#").replace("</h1>", "#")
                     out += "#" * (len(line) - 9) + "\n"
-                #Else we add a "#"
+                # Else we add a "#"
                 else:
-                    if len(line)>1:
+                    if len(line) > 1:
                         out += "#" + line
-            out+= "\n" * 2
+            out += "\n" * 2
 
     with open(fileOutput, "w+") as f:
         f.write(out)
