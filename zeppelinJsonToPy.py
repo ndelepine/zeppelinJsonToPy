832f915402ecd66e20a67004cf14f87362d4d45c
Nicolas DELEPINE <Nd.delepine@gmail.com>
2017-07-21 17:41:16 +0200

    Downgradind to Python 2.7

--- zeppelinJsonToPy.py
+++ zeppelinJsonToPy.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env
 
-## AUTHOR: Lucas Tittmann
+## AUTHOR: Lucas Tittmann & Nicolas Delepine
 ## LICENSE: Apache v 2.0
-## PYTHON_VERSION : 3.6.1
+## PYTHON_VERSION : 2.7.11
 ##
 ## DESCRIPTION
 ##   Script to transform Apache Zeppelin [https://zeppelin.apache.org/]
@@ -13,9 +13,17 @@ import sys
 import json
 
 def zeppelinJsonToPy(fileInput, fileOutput):
-    with open(fileInput, "r", encoding = "utf-8-sig") as f:
+
+    """
+    This function extracts the usefull content from a zeppelin JSON file and parse it into a Pyton script file.
+    @Params
+        fileInput = the filepath of the zeppelin JSON file
+        fileOutput = the filepath of the .py file to write
+    """
+
+    with open(fileInput, "r") as f:
         dataStr = f.readline()
-        data = json.loads(dataStr, encoding = "utf-8")
+        data = json.loads(dataStr.decode("utf-8-sig"), encoding = "utf-8")s
 
     out = ""
     for paragraph in data["paragraphs"]:
@@ -62,7 +70,7 @@ def zeppelinJsonToPy(fileInput, fileOutput):
 
 if __name__ == "__main__":
     #fileInput = sys.argv[1]
-    fileInput="C:/Users/DELEPNI/Documents/zeppelinJSON.json"
+    fileInput="MY_FILE_PATH"
     fileBaseName = os.path.splitext(fileInput)[0]
     fileOutput = fileBaseName + ".py"
     print(fileInput)
